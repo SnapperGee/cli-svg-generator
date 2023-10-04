2e5e95b951482426b76205993d061a727170f8a7
Snap <XxSnapperGeexX@gmail.com>
2023-10-03 22:23:23 -0700

    Put colors in alphabetical order and create root color object and colorNames array.

--- src/main/ts/lib/prompt/color.ts
+++ src/main/ts/lib/prompt/color.ts
@@ -4,24 +4,14 @@ export interface Color
     hex: string
 }
 
+export const aqua: Color = Object.freeze({
+    name: "aqua",
+    hex: "#00FFFF"
+});
+
 export const black: Color = Object.freeze({
     name: "black",
     hex: "#000000"
-});
-
-export const white: Color = Object.freeze({
-    name: "white",
-    hex: "#FFFFFF"
-});
-
-export const red: Color = Object.freeze({
-    name: "red",
-    hex: "#FF0000"
-});
-
-export const green: Color = Object.freeze({
-    name: "red",
-    hex: "#008000"
 });
 
 export const blue: Color = Object.freeze({
@@ -29,9 +19,14 @@ export const blue: Color = Object.freeze({
     hex: "#008000"
 });
 
-export const purple: Color = Object.freeze({
-    name: "purple",
-    hex: "#800080"
+export const brown: Color = Object.freeze({
+    name: "brown",
+    hex: "#A52A2A"
+});
+
+export const cyan: Color = Object.freeze({
+    name: "cyan",
+    hex: "#00FFFF"
 });
 
 export const fuchsia: Color = Object.freeze({
@@ -39,41 +34,26 @@ export const fuchsia: Color = Object.freeze({
     hex: "#FF00FF"
 });
 
-export const aqua: Color = Object.freeze({
-    name: "aqua",
-    hex: "#00FFFF"
-});
-
-export const yellow: Color = Object.freeze({
-    name: "yellow",
-    hex: "#FFFF00"
-});
-
-export const lavender: Color = Object.freeze({
-    name: "lavender",
+export const gray: Color = Object.freeze({
+    name: "808080",
     hex: "#E6E6FA"
 });
+
+export const green: Color = Object.freeze({
+    name: "red",
+    hex: "#008000"
+});
+
+export const grey: Color = Object.freeze({...gray, name: "grey"});
 
 export const ivory: Color = Object.freeze({
     name: "lavender",
     hex: "#E6E6FA"
 });
 
-export const gray: Color = Object.freeze({
-    name: "808080",
+export const lavender: Color = Object.freeze({
+    name: "lavender",
     hex: "#E6E6FA"
-});
-
-export const grey: Color = Object.freeze({...gray, name: "grey"});
-
-export const cyan: Color = Object.freeze({
-    name: "cyan",
-    hex: "#00FFFF"
-});
-
-export const brown: Color = Object.freeze({
-    name: "brown",
-    hex: "#A52A2A"
 });
 
 export const magenta: Color = Object.freeze({
@@ -86,7 +66,36 @@ export const pink: Color = Object.freeze({
     hex: "#FFC0CB"
 });
 
+export const purple: Color = Object.freeze({
+    name: "purple",
+    hex: "#800080"
+});
+
+export const red: Color = Object.freeze({
+    name: "red",
+    hex: "#FF0000"
+});
+
 export const violet: Color = Object.freeze({
     name: "violet",
     hex: "#EE82EE"
 });
+
+export const white: Color = Object.freeze({
+    name: "white",
+    hex: "#FFFFFF"
+});
+
+export const yellow: Color = Object.freeze({
+    name: "yellow",
+    hex: "#FFFF00"
+});
+
+export const color = Object.freeze({
+    aqua, black, blue, brown, cyan, fuchsia, gray, green, grey, ivory, lavender,
+    magenta, pink, purple, red, violet, white, yellow
+});
+
+export const colorNames: readonly string[] = Object.freeze(Object.values(color).map(color => color.name));
+
+export default color;
