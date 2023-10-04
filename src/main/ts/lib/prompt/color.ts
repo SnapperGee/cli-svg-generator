303f910b8b3077e5b296d0656d75d72add6384dd
Snap <XxSnapperGeexX@gmail.com>
2023-10-03 22:24:07 -0700

    Make colors readonly.

--- src/main/ts/lib/prompt/color.ts
+++ src/main/ts/lib/prompt/color.ts
@@ -4,89 +4,89 @@ export interface Color
     hex: string
 }
 
-export const aqua: Color = Object.freeze({
+export const aqua: Readonly<Color> = Object.freeze({
     name: "aqua",
     hex: "#00FFFF"
 });
 
-export const black: Color = Object.freeze({
+export const black: Readonly<Color> = Object.freeze({
     name: "black",
     hex: "#000000"
 });
 
-export const blue: Color = Object.freeze({
+export const blue: Readonly<Color> = Object.freeze({
     name: "blue",
     hex: "#008000"
 });
 
-export const brown: Color = Object.freeze({
+export const brown: Readonly<Color> = Object.freeze({
     name: "brown",
     hex: "#A52A2A"
 });
 
-export const cyan: Color = Object.freeze({
+export const cyan: Readonly<Color> = Object.freeze({
     name: "cyan",
     hex: "#00FFFF"
 });
 
-export const fuchsia: Color = Object.freeze({
+export const fuchsia: Readonly<Color> = Object.freeze({
     name: "fuchsia",
     hex: "#FF00FF"
 });
 
-export const gray: Color = Object.freeze({
+export const gray: Readonly<Color> = Object.freeze({
     name: "808080",
     hex: "#E6E6FA"
 });
 
-export const green: Color = Object.freeze({
+export const green: Readonly<Color> = Object.freeze({
     name: "red",
     hex: "#008000"
 });
 
-export const grey: Color = Object.freeze({...gray, name: "grey"});
+export const grey: Readonly<Color> = Object.freeze({...gray, name: "grey"});
 
-export const ivory: Color = Object.freeze({
+export const ivory: Readonly<Color> = Object.freeze({
     name: "lavender",
     hex: "#E6E6FA"
 });
 
-export const lavender: Color = Object.freeze({
+export const lavender: Readonly<Color> = Object.freeze({
     name: "lavender",
     hex: "#E6E6FA"
 });
 
-export const magenta: Color = Object.freeze({
+export const magenta: Readonly<Color> = Object.freeze({
     name: "magenta",
     hex: "#FF00FF"
 });
 
-export const pink: Color = Object.freeze({
+export const pink: Readonly<Color> = Object.freeze({
     name: "pink",
     hex: "#FFC0CB"
 });
 
-export const purple: Color = Object.freeze({
+export const purple: Readonly<Color> = Object.freeze({
     name: "purple",
     hex: "#800080"
 });
 
-export const red: Color = Object.freeze({
+export const red: Readonly<Color> = Object.freeze({
     name: "red",
     hex: "#FF0000"
 });
 
-export const violet: Color = Object.freeze({
+export const violet: Readonly<Color> = Object.freeze({
     name: "violet",
     hex: "#EE82EE"
 });
 
-export const white: Color = Object.freeze({
+export const white: Readonly<Color> = Object.freeze({
     name: "white",
     hex: "#FFFFFF"
 });
 
-export const yellow: Color = Object.freeze({
+export const yellow: Readonly<Color> = Object.freeze({
     name: "yellow",
     hex: "#FFFF00"
 });
