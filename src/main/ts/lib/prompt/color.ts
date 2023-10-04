f9fffa0b1d4f0100bdb29b16533f05c1e131ce54
Snap <XxSnapperGeexX@gmail.com>
2023-10-03 18:39:52 -0700

    Init prompt color module.

---
+++ src/main/ts/lib/prompt/color.ts
@@ -0,0 +1,82 @@
+export interface Color
+{
+    name: string,
+    hex: string
+}
+
+export const black: Color = Object.freeze({
+    name: "black",
+    hex: "#000000"
+});
+
+export const white: Color = Object.freeze({
+    name: "white",
+    hex: "#FFFFFF"
+});
+
+export const red: Color = Object.freeze({
+    name: "red",
+    hex: "#FF0000"
+});
+
+export const green: Color = Object.freeze({
+    name: "red",
+    hex: "#008000"
+});
+
+export const blue: Color = Object.freeze({
+    name: "blue",
+    hex: "#008000"
+});
+
+export const purple: Color = Object.freeze({
+    name: "purple",
+    hex: "#800080"
+});
+
+export const fuchsia: Color = Object.freeze({
+    name: "fuchsia",
+    hex: "#FF00FF"
+});
+
+export const aqua: Color = Object.freeze({
+    name: "aqua",
+    hex: "#00FFFF"
+});
+
+export const yellow: Color = Object.freeze({
+    name: "yellow",
+    hex: "#FFFF00"
+});
+
+export const lavender: Color = Object.freeze({
+    name: "lavender",
+    hex: "#E6E6FA"
+});
+
+export const ivory: Color = Object.freeze({
+    name: "lavender",
+    hex: "#E6E6FA"
+});
+
+export const gray: Color = Object.freeze({
+    name: "808080",
+    hex: "#E6E6FA"
+});
+
+export const grey: Color = Object.freeze({...gray, name: "grey"});
+
+export const cyan: Color = Object.freeze({
+    name: "cyan",
+    hex: "#00FFFF"
+});
+
+export const brown: Color = Object.freeze({
+    name: "brown",
+    hex: "#A52A2A"
+});
+
+export const magenta: Color = Object.freeze({
+    name: "magenta",
+    hex: "#FF00FF"
+});
