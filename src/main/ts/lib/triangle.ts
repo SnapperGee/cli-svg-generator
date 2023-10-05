18ebecf24c2b81498c0cba54b3f9cb1420a5bf06
Snap <XxSnapperGeexX@gmail.com>
2023-10-04 21:17:02 -0700

    Implement xml string builder in triangle module.

--- src/main/ts/lib/triangle.ts
+++ src/main/ts/lib/triangle.ts
@@ -5,14 +5,14 @@ export class Triangle extends Shape
 {
     readonly #xml: string;
 
-    public constructor(width: number, height: number, color: string, text: Text)
+    public constructor(size: number, color: string, text: Text)
     {
-        super(ShapeType.TRIANGLE, width, height, color, text);
+        super(ShapeType.TRIANGLE, size, size, color, text);
 
         this.#xml = createXML().ele("svg", {version: 1.1, xmlns: "http://www.w3.org/2000/svg"})
             .ele("polygon", {points: `0,${this.width} ${this.height},${this.width} ${this.height / 2}, 0`, fill: this.color}).up()
             .ele("text", {x: this.width / 2, y: (2/3) * this.height, "font-size": this.text.size, "text-anchor": "middle", fill: this.text.color}).txt(this.text.content)
-            .end({headless: true, prettyPrint: true});
+            .end({headless: true});
     }
 
     public override get xml(): string { return this.#xml; }
