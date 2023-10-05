e2eee383d04a8d3b77eabcc476fb2fe863530c14
Snap <XxSnapperGeexX@gmail.com>
2023-10-05 12:17:55 -0700

    Add doc comments to validate-input module.

--- src/main/ts/lib/prompt/validate-input.ts
+++ src/main/ts/lib/prompt/validate-input.ts
@@ -1,12 +1,26 @@
+/**
+ * @module validate-input
+ */
+
 import { colorNames } from "./color.js";
 
 const validHexChars: readonly string[] = Object.freeze(Array.from("ABCDEFabcdef0123456789"));
 
-export const isColor = async (valueBeingValidated: string, input: string): Promise<string | boolean> =>
+/**
+ * Verifies whether a `string` is a valid color or not. It makes sure that it's either a recognized color name or a hex value.
+ *
+ * @param typeOfValueBeingValidated A `string` used in the message returned if the input isn't a valid color.
+ *
+ * @param input The `string` being  validated for being a color.
+ *
+ * @returns Returns a promise containing `true` if the input is a valid color or `false` or a `string` message if the
+ *          input is an invalid color.
+ */
+export const isColor = async (typeOfValueBeingValidated: string, input: string): Promise<string | boolean> =>
 {
     if (input.length === 0)
     {
-        return `A ${valueBeingValidated} color name or hex value is required.`;
+        return `A ${typeOfValueBeingValidated} color name or hex value is required.`;
     }
 
     if (input.startsWith("#"))
