836c53487842fb5752997c6f5e9eedf439b650cc
Snap <XxSnapperGeexX@gmail.com>
2023-10-04 22:28:28 -0700

    Add shape questions and begin adding text size question.

--- src/main/ts/lib/prompt/question.ts
+++ src/main/ts/lib/prompt/question.ts
@@ -24,6 +24,24 @@ const textContentQuestion: Readonly<Answers> = Object.freeze({
     suffix: SUFFIX,
 });
 
+// TODO: Finish filter and validator.
+const textSizeQuestion: Readonly<Answers> = Object.freeze({
+    type: "input",
+    name: "textSize",
+    message: "Text size",
+    validate: (input: number) => new Promise((resolve) =>{
+        if (Number.isNaN(input))
+        {
+            resolve("A number is required.");
+            return;
+        }
+
+        resolve(true);
+    }),
+    prefix: PREFIX,
+    suffix: SUFFIX,
+});
+
 const textColorQuestion: Readonly<Answers> = Object.freeze({
     type: "input",
     name: "textColor",
@@ -60,16 +78,51 @@ const textColorQuestion: Readonly<Answers> = Object.freeze({
 const shapeQuestion: Readonly<Answers> = Object.freeze({
     type: "list",
     name: "shape",
-    message: "Choose SVG shape",
+    message: "Shape",
     choices: () => Promise.resolve(Object.values(ShapeType)),
     prefix: PREFIX,
     suffix: SUFFIX,
 });
 
+const shapeColorQuestion: Readonly<Answers> = Object.freeze({
+    type: "input",
+    name: "shapeColor",
+    message: "Shape color",
+    filter: (input: string) => new Promise((resolve) => {
+        const trimmedInput = input.trim();
+        resolve(trimmedInput.startsWith("#") ? trimmedInput.toUpperCase() : trimmedInput.toLowerCase());
+    }),
+    validate: (input: string) => new Promise((resolve) => {
+        if (input.length === 0)
+        {
+            resolve("A shape color name or hex value is required.");
+            return;
+        }
+
+        if (input.startsWith("#") && input.length > 7)
+        {
+            resolve("Hex value cannot consist of more than 7 characters (including leading hashtag symbol).");
+            return;
+        }
+
+        if ( ! input.startsWith("#") && ! colorNames.includes(input))
+        {
+            resolve(`Unrecognized color: "${input}". Use a hex value with a leading '#' character to use a custom color.`);
+            return;
+        }
+
+        resolve(true);
+    }),
+    prefix: PREFIX,
+    suffix: SUFFIX,
+});
+
 export const question = Object.freeze({
-    shape: shapeQuestion,
     textContent: textContentQuestion,
-    textColor: textColorQuestion
+    textSize: textSizeQuestion,
+    textColor: textColorQuestion,
+    shape: shapeQuestion,
+    shapeColor: shapeColorQuestion
 });
 
 export const questionsArray: readonly Answers[] = Object.freeze(Object.values(question));
