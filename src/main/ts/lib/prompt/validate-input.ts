4f1e7c9310bbe8714a910d334e770728c40e5b63
Snap <XxSnapperGeexX@gmail.com>
2023-10-04 23:50:09 -0700

    Check for valid hex chars in validate-input.isValidColor(string, string) method.

--- src/main/ts/lib/prompt/validate-input.ts
+++ src/main/ts/lib/prompt/validate-input.ts
@@ -1,42 +1,55 @@
 import { colorNames } from "./color.js";
 
-export const isNonNegativeNumber = (input: string): Promise<string | boolean> => {
+const validHexChars: readonly string[] = Object.freeze(Array.from("ABCDEFabcdef0123456789"));
+
+export const isNonNegativeNumber = async (input: string): Promise<string | boolean> => {
     if (input.trim().length === 0)
     {
-        return Promise.resolve("A number is required.");
+        return "A number is required.";
     }
 
     const numberInput = Number(input);
 
     if (Number.isNaN(numberInput))
     {
-        return Promise.resolve("A number is required.");
+        return "A number is required.";
     }
 
     if (numberInput < 0)
     {
-        return Promise.resolve("A non-negative number is required.");
+        return "A non-negative number is required.";
     }
 
-    return Promise.resolve(true);
+    return true;
 }
 
-export const isValidColor = (valueBeingValidated: string, input: string): Promise<string | boolean> =>
+export const isValidColor = async (valueBeingValidated: string, input: string): Promise<string | boolean> =>
 {
     if (input.length === 0)
     {
-        return Promise.resolve(`A ${valueBeingValidated} color name or hex value is required.`);
+        return `A ${valueBeingValidated} color name or hex value is required.`;
     }
 
-    if (input.startsWith("#") && input.length > 7)
+    if (input.startsWith("#"))
     {
-        return Promise.resolve("Hex value cannot consist of more than 7 characters (including leading hashtag symbol).");
+        if (input.length > 7)
+        {
+            return "Hex value cannot consist of more than 7 characters (including leading hashtag symbol).";
+        }
+
+        for (const char of input.slice(1))
+        {
+            if ( ! validHexChars.includes(char))
+            {
+                return `Hex value contains invalid character: '${char}'`;
+            }
+        }
     }
 
     if ( ! input.startsWith("#") && ! colorNames.includes(input))
     {
-        return Promise.resolve(`Unrecognized color: "${input}". Use a hex value with a leading '#' character to use a custom color.`);
+        return `Unrecognized color: "${input}". Use a hex value with a leading '#' character to use a custom color.`;
     }
 
-    return Promise.resolve(true);
+    return true;
 }
