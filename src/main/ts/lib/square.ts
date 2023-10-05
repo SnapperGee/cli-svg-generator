401932b42bf95f721d38ac1970e0385c2f078eda
Snap <XxSnapperGeexX@gmail.com>
2023-10-05 12:25:07 -0700

    Add doc comments to square module.

--- src/main/ts/lib/square.ts
+++ src/main/ts/lib/square.ts
@@ -1,9 +1,28 @@
+/**
+ * @module square
+ */
+
 import { Shape, ShapeType, Text } from "./shape.js";
 import { create as createXML } from "xmlbuilder2";
 
+/**
+ * Class used to create an SVG containing a filled square and text.
+ */
 export class Square extends Shape
 {
     readonly #xml: string;
+
+    /**
+     * Class for generating a square SVG with text located in the center of it.
+     *
+     * @param width The width of the generated SVG.
+     *
+     * @param height The height of the generated SVG.
+     *
+     * @param color The color to fill the generated square with.
+     *
+     * @param text The text centered in the square.
+     */
     public constructor(width: number, height: number, color: string, text: Text)
     {
         super(ShapeType.SQUARE, width, height, color, text);
@@ -16,9 +35,23 @@ export class Square extends Shape
             .end({headless: true});
     }
 
+    /**
+     * Getter for the xml `string` of the SVG.
+     */
     public override get xml(): string { return this.#xml; }
 }
 
+/**
+ * Creates a {@link Square} object with a width and height of 300px by 200px.
+ *
+ * @param shapeColor A color hex or name value to use as the rectangle fill.
+ *
+ * @param text The text located in the center of the square.
+ *
+ * @param textColor The color of the text.
+ *
+ * @returns A {@link Square} object with a preset width and height of 300px x 200px.
+ */
 export const create300x200Square = (shapeColor: string, text: string, textColor: string): Square => new Square(300, 200, shapeColor, {content: text, size: 60, color: textColor});
 
 export default Square;
