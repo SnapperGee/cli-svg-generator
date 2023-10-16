c2888de28190ef9d059be771f8e21cf02048c7f9
Snap <XxSnapperGeexX@gmail.com>
2023-10-16 01:15:25 -0700

    Delete jest and babel dependencies.

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { type Config } from 'jest';
-
-export default async (): Promise<Config> => ({
-    moduleDirectories: ["node_modules", "src"],
-    modulePaths: ["<rootDir>"],
-    roots: ["<rootDir>"]
-})
